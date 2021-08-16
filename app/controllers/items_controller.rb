e810d533554501d3cd1d69f843bf7ba27f9a273a
Yusuke0507 <xitianxiongjie0@gmail.com>
2021-08-16 14:08:37 +0900

    最終課題_修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
-  before_action :move_to_root_path, only: [:edit, :update]
 
   def index
     @items = Item.all.order(id: "DESC" )
